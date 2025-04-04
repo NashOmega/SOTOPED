d8b697a902ba1f3d3134c536d9fbb98c6f8a1ca8
Bernard DEKPE <bdekpe@neosys.fr>
2025-04-04 19:12:48 +0000

    fjdks

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_BASE_URL: "http://localhost:5059",
+  API_BASE_URL: "https://sotoped-backend.onrender.com",
   Certificate_URL: "Stoped/code",
   Certificate_Download_URL: "Stoped/GenerateCertificateByCiteria",
   Spectator_URL: "Stoped",
